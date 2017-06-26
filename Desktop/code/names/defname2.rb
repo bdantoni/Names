b571589a65a9fa5ea603911bdd272c203c35e30f
Bailey DAntoni <bkdmarshall@hotmail.com>
2017-06-26 13:35:31 -0400

    first name, last name, full name in methods

---
+++ Desktop/code/names/defname2.rb
@@ -0,0 +1,20 @@
+def first_name(name1)
+	puts "name1: #{name1}"
+
+end
+
+def last_name(name2)
+	puts "name2: #{name2}"
+
+end
+
+def whole_name(name3)
+	puts "name3: #{name3}"
+
+end
+
+first_name("Bailey")
+
+last_name("DAntoni")
+
+whole_name("Bailey DAntoni")
